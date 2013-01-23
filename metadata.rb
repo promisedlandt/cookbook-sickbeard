6ab3430c3158fd90ddacb9b3e17e9cddc573841c
Nils Landt <nils@promisedlandt.de>
2013-01-23 01:04:45 +0100

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cookbooks@promisedlandt.de"
 license          "All rights reserved"
 description      "Installs/Configures Sick Beard PVR"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
 depends "python"
 depends "git"
